cd48aa8c7c46b6700477a8ae16f60e161804f587
prathyanthDhanus <prathyanthbusinessmail@gmail.com>
2024-03-19 02:58:04 +0530

    feat:implemented navbar and home

---
+++ src/Pages/Landingpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import Home from './Home/Home'
+const Landingpage = () => {
+  return (
+    <div>
+  <Navbar/>
+  <Home/>
+    </div>
+  )
+}
+
+export default Landingpage
